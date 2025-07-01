6521d5ac2f3b7015b967e7ce913a92ac56fbd6ee
nivya-2 <you@example.com>
2025-07-01 14:33:57 +0530

    Added libs

---
+++ libs/directives/src/lib/directives/directives.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-directives',
+  imports: [CommonModule],
+  templateUrl: './directives.html',
+  styleUrl: './directives.css',
+})
+export class Directives {}
