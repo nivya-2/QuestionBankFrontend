0f6c97d34376dab1981bff062d2735844a7e7aa9
Dhunan <midhun2017.com>
2025-07-03 12:40:47 +0530

    feat: Task 201 - Submit for PR

--- apps/question-bank/src/app/app.routes.ts
+++ apps/question-bank/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { ListInterview } from './features/interview-management/list-interview/list-interview';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+    path: '',
+    redirectTo: 'interviews',
+    pathMatch: 'full'
+  },
+  {
+    path: 'interviews',
+    component: ListInterview
+  },
+];
